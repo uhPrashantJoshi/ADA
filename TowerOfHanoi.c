d8636f25e39749b9275d2a75ecbf5ba1d7a8c4e3
Prashant R Joshi <87548699+uhPrashantJoshi@users.noreply.github.com>
2022-07-24 19:59:48 +0530

    Update TowerOfHanoi.c

--- TowerOfHanoi.c
+++ TowerOfHanoi.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void towerofhanoi(int n, char start, char dest, char aux)
+void TowerOfHanoi(int n, char start, char dest, char aux)
 {
     if(n==1)
     {
-        printf("\nMove DISK 1 from %c to %c", start,dest);
+        printf("Move disk 1 from %c to %c", start, dest);
         return;
+
     }
-    else
-    {
-        towerofhanoi(n-1,start,aux,dest);
-        printf("\nMove DISH %d from %c to %c", n , start, dest);
-        towerofhanoi(n-1,aux,dest,start);
+    else{
+        TowerOfHanoi(n-1,start,aux,dest);
+        printf("Move disk %d from %c to %c",n,start,dest);
+        TowerOfHanoi(n-1,aux,dest,start);
     }
 }
 
 int main()
 {
-    towerofhanoi(3,'A','B','C');
+    TowerOfHanoi(3,'A','C','B');
     return 0;
 }
